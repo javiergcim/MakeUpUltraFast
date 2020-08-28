c06005fff52f530cdb14205629d0630a09c57c22
Javier Garduño <javier@cuadrodigital.net>
2020-08-27 23:52:00 -0500

    Flashing mobs artifact

--- shaders/lib/tone_maps.glsl
+++ shaders/lib/tone_maps.glsl
@@ -4,4 +4,5 @@ Tone map related functions.
 
 vec3 tonemap(vec3 x){
   return x / pow(pow(x, vec3(4.0)) + 1.0, vec3(.25));
+  // return 2.0 * (pow((1.0 + exp(-(x * 4.5 - 3.434))), vec3(-.2))) - 1;
 }
