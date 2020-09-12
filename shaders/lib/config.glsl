a093ea93c53c73cecbb996dbecb99aa64bd31002
Javier Garduño <javier@cuadrodigital.net>
2020-09-12 02:14:51 -0500

    RC II v1.54

--- shaders/lib/config.glsl
+++ shaders/lib/config.glsl
@@ -28,5 +28,5 @@ Javier Garduño - GNU Lesser General Public License v3.0
 #define DOF_STRENGTH 50.0  // [20.0 25.0 30.0 35.0 40.0 45.0 50.0 55.0 60.0 65.0 70.0 75.0 80.0]  Depth of field strenght.
 #define AO 1  // [0 1] Turn on for enhanced ambient occlusion (medium performance impact).
 #define AOSTEPS 8 // [8 12 16 20] How many samples are taken. More samples, less performance
-#define AO_STRENGHT 0.6 // [0.4 0.5 0.6 0.7 0.8 0.9 1.0] Ambient oclusion strenght
+#define AO_STRENGHT 0.7 // [0.4 0.5 0.6 0.7 0.8 0.9 1.0] Ambient oclusion strenght
 #define RT_SAMPLES 10 // [6 8 10 12 14 16 18 20 22 24] Reflections samples (only raytrace). More samples, less performance.
