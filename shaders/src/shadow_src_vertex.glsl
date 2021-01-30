b8edf397b0877780ba948b716b41743662824d5e
Javier Garduño <javier@cuadrodigital.net>
2021-01-30 01:37:34 -0600

    The End Shadow bug fixed

--- shaders/src/shadow_src_vertex.glsl
+++ shaders/src/shadow_src_vertex.glsl
@@ -1,16 +1,60 @@
 #ifdef FOLIAGE_V
   float NdotL;
   if (is_foliage > .2) {
-    NdotL = clamp(
-      max(
-        dot(normal, normalize(shadowLightPosition)),
-        dot(-normal, normalize(shadowLightPosition))
-      ),
+    #ifdef THE_END
+      vec3 custom_light_pos = normalize(gbufferModelView * vec4(0.0, 0.89442719, 0.4472136, 0.0)).xyz;
+      NdotL = clamp(
+        max(
+          dot(normal, custom_light_pos),
+          dot(-normal, custom_light_pos)
+        ),
+        0.0,
+        1.0
+        );
+    #else
+      vec3 normal_light_pos = normalize(shadowLightPosition);
+      NdotL = clamp(
+        max(
+          dot(normal, normal_light_pos),
+          dot(-normal, normal_light_pos)
+        ),
+        0.0,
+        1.0
+        );
+    #endif
+  } else {
+    #ifdef THE_END
+      NdotL = clamp(
+        dot(
+          normal,
+          normalize(gbufferModelView * vec4(0.0, 0.89442719, 0.4472136, 0.0)).xyz
+          ),
+        0.0,
+        1.0
+        );
+    #else
+      NdotL = clamp(
+        dot(
+          normal,
+          normalize(shadowLightPosition)
+          ),
+        0.0,
+        1.0
+        );
+    #endif
+  }
+#else
+  #ifdef THE_END
+    float NdotL = clamp(
+      dot(
+        normal,
+        normalize(gbufferModelView * vec4(0.0, 0.89442719, 0.4472136, 0.0)).xyz
+        ),
       0.0,
       1.0
       );
-  } else {
-    NdotL = clamp(
+  #else
+    float NdotL = clamp(
       dot(
         normal,
         normalize(shadowLightPosition)
@@ -18,16 +62,7 @@
       0.0,
       1.0
       );
-  }
-#else
-  float NdotL = clamp(
-    dot(
-      normal,
-      normalize(shadowLightPosition)
-      ),
-    0.0,
-    1.0
-    );
+  #endif
 #endif
 
 shadow_pos = get_shadow_pos(position, NdotL);
