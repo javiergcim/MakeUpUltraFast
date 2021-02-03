1eeffb1a3fc0b4628a0bb1959bc88ad1ed8c55c3
Javier Garduño <javierg.cim@gmail.com>
2021-02-03 00:21:20 -0600

    Noise test

--- shaders/lib/dither.glsl
+++ shaders/lib/dither.glsl
@@ -17,16 +17,28 @@ float dither_grad_noise(vec2 p) {
   return fract(52.9829189 * fract(0.06711056 * p.x + 0.00583715 * p.y));
 }
 
-float texture_noise_64(vec2 p, sampler2D noise) {
-  return texture2D(noise, p * 0.015625).r;
-}
-
 // float hash11(float p) {
 //   p = fract(p * .1031);
 //   p *= p + 33.33;
 //   p *= p + p;
 //   return fract(p);
 // }
+
+vec2 hash21(float p) {
+  vec3 p3 = fract(vec3(p) * vec3(.1031, .1030, .0973));
+  p3 += dot(p3, p3.yzx + 33.33);
+  return fract((p3.xx+p3.yz)*p3.zy);
+}
+
+float texture_noise_64(vec2 p, sampler2D noise) {
+  return texture2D(noise, p * 0.015625).r;
+}
+
+float timed_texture_noise_64(vec2 p, sampler2D noise) {
+  vec2 dither = hash21(frameTimeCounter);
+  return texture2D(noise, p * 0.015625 + dither).r;
+}
+
 //
 // float timed_hash11(float p) {
 //   p = fract((p + frameTimeCounter) * .1031);
@@ -46,12 +58,6 @@ float timed_hash12(vec2 p) {
   p3 += dot(p3, p3.yzx + 33.33);
   return fract((p3.x + p3.y) * p3.z);
 }
-
-// vec2 hash21(float p) {
-//   vec3 p3 = fract(vec3(p) * vec3(.1031, .1030, .0973));
-//   p3 += dot(p3, p3.yzx + 33.33);
-//   return fract((p3.xx+p3.yz)*p3.zy);
-// }
 
 // vec2 hash22(vec2 p) {
 //   vec3 p3 = fract(vec3(p.xyx) * vec3(.1031, .1030, .0973));
@@ -73,7 +79,7 @@ float bayer2(vec2 a) {
 #define bayer4(a)   (bayer2(.5 * (a)) * .25+ bayer2(a))
 #define bayer8(a)   (bayer4(.5 * (a)) * .25+ bayer2(a))
 #define bayer16(a)  (bayer8(.5 * (a)) * .25+ bayer2(a))
-// 
+//
 // #define bayer64(a)  (bayer32(.5 * (a)) * .25+ bayer2(a))
 // #define bayer128(a) (bayer64(.5 * (a)) * 0.25 + bayer2(a))
 // #define bayer256(a) (bayer128(.5 * (a)) * 0.25 + bayer2(a))
