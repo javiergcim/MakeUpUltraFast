8c45ed017e810c1193f4e3f8ed131bb4328069f1
Javier Garduño <javierg.cim@gmail.com>
2021-10-18 01:55:31 -0500

    MC_VERSION removed

--- shaders/src/writebuffers.glsl
+++ shaders/src/writebuffers.glsl
@@ -1,37 +1,3 @@
-// #ifdef WATER_F
-//   /* DRAWBUFFERS:1 */
-//   gl_FragData[0] = block_color;
-// #elif defined CLOUDS_SHADER
-//   /* DRAWBUFFERS:1 */
-//   gl_FragData[0] = block_color;
-// #else
-//   #ifdef SET_FOG_COLOR
-//     /* DRAWBUFFERS:07 */
-//     gl_FragData[0] = block_color;
-//     #if defined THE_END || defined NETHER
-//       gl_FragData[1] = vec4(background_color, 1.0);
-//     #else
-//       gl_FragData[1] = vec4(background_color, 0.0);
-//     #endif
-//   #else
-//     /* DRAWBUFFERS:0 */
-//     gl_FragData[0] = block_color;
-//   #endif
-// #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #ifdef WATER_F
   /* DRAWBUFFERS:1 */
   gl_FragData[0] = block_color;
--- shaders/world-1/gbuffers_textured.fsh
+++ shaders/world-1/gbuffers_textured.fsh
@@ -7,9 +7,7 @@ Javier Garduño - GNU Lesser General Public License v3.0
 
 #define NETHER
 #define GBUFFER_TEXTURED
-#if MC_VERSION >= 11300
-  #define CLOUDS_SHADER
-#endif
+#define CLOUDS_SHADER
 #define NO_SHADOWS
 
 #include "/common/solid_blocks_fragment.glsl"
