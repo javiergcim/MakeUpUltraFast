704e7637862499b91f8da3631d5b69ef971f0d8b
Javier Garduño <javierg.cim@gmail.com>
2021-10-18 00:11:32 -0500

    Writebuffers

--- shaders/src/writebuffers.glsl
+++ shaders/src/writebuffers.glsl
@@ -1,14 +1,53 @@
+// #ifdef WATER_F
+//   /* DRAWBUFFERS:1 */
+//   gl_FragData[0] = block_color;
+// #elif defined CLOUDS_SHADER
+//   /* DRAWBUFFERS:1 */
+//   gl_FragData[0] = block_color;
+// #else
+//   #ifdef SET_FOG_COLOR
+//     /* DRAWBUFFERS:07 */
+//     gl_FragData[0] = block_color;
+//     #if defined THE_END || defined NETHER
+//       gl_FragData[1] = vec4(background_color, 1.0);
+//     #else
+//       gl_FragData[1] = vec4(background_color, 0.0);
+//     #endif
+//   #else
+//     /* DRAWBUFFERS:0 */
+//     gl_FragData[0] = block_color;
+//   #endif
+// #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #ifdef WATER_F
   /* DRAWBUFFERS:1 */
   gl_FragData[0] = block_color;
 #elif defined CLOUDS_SHADER
-  /* DRAWBUFFERS:1 */
-  gl_FragData[0] = block_color;
+  #if MC_VERSION >= 11300
+    /* DRAWBUFFERS:1 */
+    gl_FragData[0] = block_color;
+  #else
+    /* DRAWBUFFERS:0 */
+    gl_FragData[0] = block_color;
+  #endif
 #else
   #ifdef SET_FOG_COLOR
     /* DRAWBUFFERS:07 */
     gl_FragData[0] = block_color;
-    #if defined THE_END || defined NETHER
+    #ifdef THE_END
       gl_FragData[1] = vec4(background_color, 1.0);
     #else
       gl_FragData[1] = vec4(background_color, 0.0);
@@ -18,3 +57,4 @@
     gl_FragData[0] = block_color;
   #endif
 #endif
+
