5d95057a08a2dc0682f7b1900745190335fcc4c2
Javier Garduño <javierg.cim@gmail.com>
2021-12-19 23:39:20 -0600

    Elementos en "basic" ya no brillan en la noche

--- shaders/common/basic_blocks_fragment.glsl
+++ shaders/common/basic_blocks_fragment.glsl
@@ -16,9 +16,12 @@ uniform float alphaTestRef;
 
 in vec4 tint_color;
 in vec2 texcoord;
+in float basic_light;
 
 void main() {
   vec4 block_color = tint_color;
+
+  block_color.rgb *= basic_light;
 
   if(block_color.a < alphaTestRef) discard;  // Full transparency
   #include "/src/writebuffers.glsl"
--- shaders/common/basic_blocks_vertex.glsl
+++ shaders/common/basic_blocks_vertex.glsl
@@ -11,13 +11,18 @@
 uniform vec3 chunkOffset;
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
+uniform int current_hour_floor;
+uniform int current_hour_ceil;
+uniform float current_hour_fract;
 
+in ivec2 vaUV2;  // Light coordinates
 in vec2 vaUV0;  // Texture coords
 in vec4 vaColor;
 in vec3 vaPosition;
 
 out vec4 tint_color;
 out vec2 texcoord;
+out float basic_light;
 
 #include "/lib/luma.glsl"
 #include "/lib/basic_utils.glsl"
@@ -30,4 +35,16 @@ void main() {
   #include "/src/basiccoords_vertex.glsl"
   #include "/src/position_vertex.glsl"
   tint_color = vaColor;
+
+  vec2 lmcoord = vec2(vaUV2) * 0.0041841004184100415;
+
+  vec2 basic_light_2 = (max(lmcoord, vec2(0.065)) - vec2(0.065)) * 1.06951871657754;
+
+  basic_light = (mix(
+      ambient_exposure[current_hour_floor],
+      ambient_exposure[current_hour_ceil],
+      current_hour_fract
+    ) + .05) * basic_light_2.y;
+
+  basic_light = clamp((basic_light_2.x * 0.2) + basic_light, 0.0, 1.0);
 }
