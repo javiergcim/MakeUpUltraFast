f2471493cf4df4f69613e0df3064849545cb1069
Javier Garduño <javierg.cim@gmail.com>
2021-12-20 00:45:14 -0600

    Se corrigen elementos dibujados por "basic"

--- shaders/common/basic_blocks_fragment.glsl
+++ shaders/common/basic_blocks_fragment.glsl
@@ -11,9 +11,12 @@
 /* Config, uniforms, ins, outs */
 varying vec4 tint_color;
 varying vec2 texcoord;
+varying float basic_light;
 
 void main() {
   vec4 block_color = tint_color;
+
+  block_color.rgb *= basic_light;
 
   #include "/src/writebuffers.glsl"
 }
--- shaders/common/basic_blocks_vertex.glsl
+++ shaders/common/basic_blocks_vertex.glsl
@@ -11,9 +11,13 @@
 uniform vec3 chunkOffset;
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
+uniform int current_hour_floor;
+uniform int current_hour_ceil;
+uniform float current_hour_fract;
 
 varying vec4 tint_color;
 varying vec2 texcoord;
+varying float basic_light;
 
 #include "/lib/luma.glsl"
 #include "/lib/basic_utils.glsl"
@@ -26,4 +30,16 @@ void main() {
   #include "/src/basiccoords_vertex.glsl"
   #include "/src/position_vertex.glsl"
   tint_color = gl_Color;
+
+  vec2 lmcoord = (gl_TextureMatrix[1] * gl_MultiTexCoord1).xy * 1.0323886639676114;
+
+  vec2 basic_light_2 = (max(lmcoord, vec2(0.065)) - vec2(0.065)) * 1.06951871657754;
+
+  basic_light = (mix(
+      ambient_exposure[current_hour_floor],
+      ambient_exposure[current_hour_ceil],
+      current_hour_fract
+    ) + .05) * basic_light_2.y;
+
+  basic_light = clamp((basic_light_2.x * 0.2) + basic_light, 0.0, 1.0);
 }
