017e8ba109719d1580cab20dc67da7afb9ed0741
Javier Garduño <javierg.cim@gmail.com>
2021-12-19 22:10:48 -0600

    Ajustado el decaimiento de AO

--- shaders/common/deferred_fragment.glsl
+++ shaders/common/deferred_fragment.glsl
@@ -148,20 +148,22 @@ void main() {
   #endif
 
   #if AO == 1
-    // AO distance attenuation
     #if defined VOL_LIGHT && defined SHADOW_CASTING && !defined NETHER
-      float fog_density_coeff = FOG_DENSITY;
+      float fog_density_coeff = FOG_DENSITY * FOG_ADJUST;
     #else
       float fog_density_coeff = mix(
         fog_density[current_hour_floor],
         fog_density[current_hour_ceil],
         current_hour_fract
-        );
+        ) * FOG_ADJUST;
     #endif
 
+    // AO distance attenuation
+    float fog_intensity_coeff = eyeBrightnessSmooth.y * 0.004166666666666667;
+
     float ao_att = pow(
-      linear_d,
-      mix(fog_density_coeff * .5, .25, rainStrength)
+      clamp(linear_d * 1.25, 0.0, 1.0) * fog_intensity_coeff,
+      mix(fog_density_coeff, .5, rainStrength)
     );
 
     float final_ao = mix(dbao(dither), 1.0, ao_att);
