ee81473315390cfa1e64ff2b79cf513f83e734d2
Javier Garduño <javierg.cim@gmail.com>
2021-12-27 12:09:09 -0600

    Ajustes en blur

--- shaders/src/shadow_src_vertex.glsl
+++ shaders/src/shadow_src_vertex.glsl
@@ -70,4 +70,4 @@ shadow_diffuse = max(
   abs((shadow_pos.x - .5) * 2.0), abs((shadow_pos.y - .5) * 2.0)
   );
 
-shadow_diffuse = pow(shadow_diffuse, 10.0);
+shadow_diffuse = pow(shadow_diffuse, 8.0);
