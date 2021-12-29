fba24b97fbdbf237b7637b17e93345b65d2b446d
Javier Garduño <javierg.cim@gmail.com>
2021-12-29 00:57:42 -0600

    Tonemap

--- shaders/lib/tone_maps.glsl
+++ shaders/lib/tone_maps.glsl
@@ -4,56 +4,34 @@ Tonemap functions.
 Javier Garduño - GNU Lesser General Public License v3.0
 */
 
-// vec3 lottes_tonemap(vec3 x, float hdrMax) {
-//     // Lottes 2016, "Advanced Techniques and Optimization of HDR Color Pipelines"
-//     // float a = 1.5;
-//     // float d = 0.977;
-//     // const float hdrMax = 8.0;
-//     // float midIn = 0.2;
-//     // float midOut = 0.27;
-
-//     float pow_a = pow(hdrMax, 1.5);
-//     float pow_b = pow(hdrMax, 1.4655);
-//     float producto_a = (pow_b - 0.0945495483551584) * 0.27;
-
-//     float b =
-//         (-0.0894427190999916 + pow_a * 0.27) /
-//         producto_a;
-//     float c =
-//         (pow_b * 0.0894427190999916 - pow_a * 0.02552837805589277) /
-//         producto_a;
-
-//     return pow(x, vec3(1.5)) / (pow(x, vec3(1.4655)) * vec3(b) + vec3(c));
-// }
-
 vec3 lottes_tonemap(vec3 x, float hdrMax) {
     // Lottes 2016, "Advanced Techniques and Optimization of HDR Color Pipelines"
-    // float a = 1.7;
+    // float a = 1.5;
     // float d = 0.977;
     // const float hdrMax = 8.0;
-    // float midIn = 0.25;
+    // float midIn = 0.23;
     // float midOut = 0.3;
 
-    float pow_a = pow(hdrMax, 1.7);
-    float pow_b = pow(hdrMax, 1.6608999999999998);
-    float producto_a = (pow_b - 0.1000088792551845) * 0.3;
+    float pow_a = pow(hdrMax, 1.5);
+    float pow_b = pow(hdrMax, 1.4655);
+    float producto_a = (pow_b - 0.11604118194392006) * 0.3;
 
     float b =
-      (-0.09473228540689989 + pow_a * 0.3) /
+      (-0.11030412503619255 + pow_a * 0.3) /
       producto_a;
     float c =
-      (pow_b * 0.09473228540689989 - pow_a * 0.030002663776555347) /
+      (pow_b * 0.11030412503619255 - pow_a * 0.03481235458317602) /
       producto_a;
 
-    return pow(x, vec3(1.7)) / (pow(x, vec3(1.6608999999999998)) * vec3(b) + vec3(c));
+    return pow(x, vec3(1.5)) / (pow(x, vec3(1.4655)) * vec3(b) + vec3(c));
 }
 
 // vec3 lottes_tonemap(vec3 x, float hdrMax) {
 //     // Lottes 2016, "Advanced Techniques and Optimization of HDR Color Pipelines"
-//     float a = 1.7;
+//     float a = 1.5;
 //     float d = 0.977;
 //     // const float hdrMax = 8.0;
-//     float midIn = 0.25;
+//     float midIn = 0.23;
 //     float midOut = 0.3;
 
 //     // Can be precomputed
