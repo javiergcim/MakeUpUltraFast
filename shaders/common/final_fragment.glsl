dad7f4e272440f5eb2af64a5cf4db0f99743d094
Javier Garduño <javierg.cim@gmail.com>
2022-03-08 17:44:25 -0600

    Debug release

--- shaders/common/final_fragment.glsl
+++ shaders/common/final_fragment.glsl
@@ -4,6 +4,14 @@ out vec4 outColor0;
 /* Config, uniforms, ins, outs */
 #include "/lib/config.glsl"
 
+#ifdef THE_END
+  #include "/lib/color_utils_end.glsl"
+#elif defined NETHER
+  #include "/lib/color_utils_nether.glsl"
+#else
+  #include "/lib/color_utils.glsl"
+#endif
+
 // Do not remove comments. It works!
 /*
 
@@ -65,6 +73,10 @@ const bool gaux4Clear = false;
 
 // 'Global' constants from system
 uniform sampler2D colortex0;
+uniform ivec2 eyeBrightnessSmooth;
+uniform int current_hour_floor;
+uniform int current_hour_ceil;
+uniform float current_hour_fract;
 
 #ifdef DEBUG_MODE
   uniform sampler2D shadowtex1;
@@ -74,7 +86,7 @@ uniform sampler2D colortex0;
 
 // Varyings (per thread shared variables)
 in vec2 texcoord;
-flat in float exposure;
+// flat in float exposure;
 
 #include "/lib/basic_utils.glsl"
 #include "/lib/tone_maps.glsl"
@@ -83,35 +95,56 @@ flat in float exposure;
   #include "/lib/aberration.glsl"
 #endif
 
-#ifdef DEBUG_MODE
-  void main() {
-    vec3 block_color;
+void main() {
+  vec2 eye_bright_smooth = vec2(eyeBrightnessSmooth);
+
+  // Tonemaping ---
+  // x: Block, y: Sky ---
+  float candle_bright = eye_bright_smooth.x * 0.0003125;
+  float exposure_coef =
+    mix(
+      ambient_exposure[current_hour_floor],
+      ambient_exposure[current_hour_ceil],
+      current_hour_fract
+    );
+  float camera_exposure =
+    ((eye_bright_smooth.y * 0.004166666666666667) * exposure_coef) + candle_bright;
+
+  // Map from 1.0 - 0.0 to 1.0 - 3.4
+  camera_exposure = (camera_exposure * -2.4) + 3.4;
+
+  #if CHROMA_ABER == 1
+    vec3 block_color = color_aberration();
+  #else
+    vec3 block_color = texture(colortex0, texcoord).rgb;
+  #endif
+
+  block_color *= vec3(camera_exposure);
+  // block_color = lottes_tonemap(block_color, exposure + 0.6);
+  block_color = custom_ACES(block_color);
+
+
+  #ifdef DEBUG_MODE
+    // vec3 block_color;
     if (texcoord.x < 0.5 && texcoord.y < 0.5) {
-      block_color = texture(shadowtex1, texcoord * 2.0).rrr;
-    } else if(texcoord.x >= 0.5 && texcoord.y >= 0.5) {
-      block_color = texture(shadowcolor0, ((texcoord - vec2(0.5)) * 2.0)).aaa;
+      // block_color = texture(shadowtex1, texcoord * 2.0).rrr;
+      block_color = vec3(camera_exposure / 4.0);
+    } else if (texcoord.x >= 0.5 && texcoord.y >= 0.5) {
+      // block_color = texture(shadowcolor0, ((texcoord - vec2(0.5)) * 2.0)).aaa;
+      block_color = vec3(eye_bright_smooth.y / 240.0);
     } else if (texcoord.x < 0.5 && texcoord.y >= 0.5) {
-      block_color = texture(colortex3, ((texcoord - vec2(0.0, 0.5)) * 2.0)).rgb;
+      block_color = texture(colortex0, ((texcoord - vec2(0.0, 0.5)) * 2.0)).rgb * vec3(camera_exposure);
+      block_color = custom_ACES(block_color);
     } else if (texcoord.x >= 0.5 && texcoord.y < 0.5) {
-      block_color = texture(shadowcolor0, ((texcoord - vec2(0.5, 0.0)) * 2.0)).rgb;
+      // block_color = texture(shadowcolor0, ((texcoord - vec2(0.5, 0.0)) * 2.0)).rgb;
+      block_color = vec3(eye_bright_smooth.x / 240.0);
     } else {
-      block_color = vec3(0.0);
+      block_color = vec3(0.5);
     }
 
     outColor0 = vec4(block_color, 1.0);
-  }
-#else
-  void main() {
-    #if CHROMA_ABER == 1
-      vec3 block_color = color_aberration();
-    #else
-      vec3 block_color = texture(colortex0, texcoord).rgb;
-    #endif
 
-    block_color *= exposure;
-    // block_color = lottes_tonemap(block_color, exposure + 0.6);
-    block_color = custom_ACES(block_color);
-
+  #else
     outColor0 = vec4(block_color, 1.0);
-  }
-#endif
+  #endif
+}
