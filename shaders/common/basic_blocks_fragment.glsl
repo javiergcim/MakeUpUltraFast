204792821c70b8f28b5c203954961c3840677936
Javier Garduño <javierg.cim@gmail.com>
2022-04-15 18:15:18 -0500

    Removed not nedeed code

--- shaders/common/basic_blocks_fragment.glsl
+++ shaders/common/basic_blocks_fragment.glsl
@@ -1,13 +1,5 @@
 #include "/lib/config.glsl"
 
-#ifdef THE_END
-  #include "/lib/color_utils_end.glsl"
-#elif defined NETHER
-  #include "/lib/color_utils_nether.glsl"
-#else
-  #include "/lib/color_utils.glsl"
-#endif
-
 /* Config, uniforms, ins, outs */
 varying vec4 tint_color;
 varying vec2 texcoord;
