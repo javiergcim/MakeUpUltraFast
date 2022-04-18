44aa0026b26c06cf13982dfdefb26ee237fb4d94
Javier Garduño <javierg.cim@gmail.com>
2022-04-18 00:54:26 -0500

    VI

--- shaders/common/line_blocks_vertex.glsl
+++ shaders/common/line_blocks_vertex.glsl
@@ -1,17 +1,6 @@
 #include "/lib/config.glsl"
 
-#ifdef THE_END
-  #include "/lib/color_utils_end.glsl"
-#elif defined NETHER
-  #include "/lib/color_utils_nether.glsl"
-#else
-  #include "/lib/color_utils.glsl"
-#endif
-
 varying vec4 tint_color;
-
-#include "/lib/luma.glsl"
-#include "/lib/basic_utils.glsl"
 
 #if AA_TYPE > 0
   #include "/src/taa_offset.glsl"
