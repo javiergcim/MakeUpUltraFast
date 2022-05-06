21653cd5f3f4d1dec7da9f493614229977fe982d
Javier Garduño <javierg.cim@gmail.com>
2022-05-06 00:39:50 -0500

    Dithers

--- shaders/common/composite_fragment.glsl
+++ shaders/common/composite_fragment.glsl
@@ -116,7 +116,7 @@ void main() {
     mat4 modeli_times_projectioni = gbufferModelViewInverse * gbufferProjectionInverse;
 
     #if AA_TYPE > 0
-      float dither = shifted_eclectic_makeup_dither(gl_FragCoord.xy);
+      float dither = shifted_eclectic_r_dither(gl_FragCoord.xy);
     #else
       float dither = eclectic_r_dither(gl_FragCoord.xy);
     #endif
