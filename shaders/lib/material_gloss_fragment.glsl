92bc6802f8267618df79ee06016a1c07842cf7f0
Javier Garduño <javierg.cim@gmail.com>
2022-06-02 15:17:00 -0500

    Color schemes 8.0

--- shaders/lib/material_gloss_fragment.glsl
+++ shaders/lib/material_gloss_fragment.glsl
@@ -3,7 +3,7 @@
     vec3 astro_pos = (gbufferModelView * vec4(0.0, 0.89442719, 0.4472136, 0.0)).xyz;
     float astro_vector =
       max(dot(normalize(reflected_vector), normalize(astro_pos)), 0.0) *
-      step(0.0, dot(astro_pos, flat_normal));
+      step(0.0001, dot(astro_pos, flat_normal));
 
     return clamp(
         mix(0.0, 1.0, pow(clamp(astro_vector * 2.0 - 1.0, 0.0, 1.0), gloss_power)),
@@ -18,7 +18,7 @@
     vec3 astro_pos = mix(-sunPosition, sunPosition, light_mix);
     float astro_vector =
       max(dot(normalize(reflected_vector), normalize(astro_pos)), 0.0) *
-      step(0.0, dot(astro_pos, flat_normal));
+      step(0.0001, dot(astro_pos, flat_normal));
 
     return clamp(
         mix(0.0, 1.0, pow(clamp(astro_vector * 2.0 - 1.0, 0.0, 1.0), gloss_power)) *
