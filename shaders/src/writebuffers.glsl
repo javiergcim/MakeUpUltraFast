47068e7f0e4af0a3ad2217dd7b8388076004f2fc
Javier Garduño <javierg.cim@gmail.com>
2022-06-27 18:12:09 -0500

    Shadows debug - particles

--- shaders/src/writebuffers.glsl
+++ shaders/src/writebuffers.glsl
@@ -2,9 +2,8 @@
   /* DRAWBUFFERS:1 */
   gl_FragData[0] = block_color;
 #elif (defined PARTICLE_SHADER && MC_VERSION >= 11300)
-  /* DRAWBUFFERS:01 */
+  /* DRAWBUFFERS:1 */
   gl_FragData[0] = block_color;
-  gl_FragData[1] = block_color;
 #elif defined GBUFFER_HAND_WATER
   /* DRAWBUFFERS:1 */
   gl_FragData[0] = block_color;
