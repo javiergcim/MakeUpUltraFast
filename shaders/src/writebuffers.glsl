edca0b60a09026409549c699b057eb9579d96b50
Javier Garduño <javierg.cim@gmail.com>
2022-06-27 19:45:52 -0500

    Version 8.5a

--- shaders/src/writebuffers.glsl
+++ shaders/src/writebuffers.glsl
@@ -2,8 +2,9 @@
   /* DRAWBUFFERS:1 */
   gl_FragData[0] = block_color;
 #elif (defined PARTICLE_SHADER && MC_VERSION >= 11300)
-  /* DRAWBUFFERS:1 */
+  /* DRAWBUFFERS:01 */
   gl_FragData[0] = block_color;
+  gl_FragData[1] = block_color;
 #elif defined GBUFFER_HAND_WATER
   /* DRAWBUFFERS:1 */
   gl_FragData[0] = block_color;
