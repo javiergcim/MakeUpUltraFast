ccb7081a30726d086ef0ddecc0bb514549402efd
Javier Garduño <javierg.cim@gmail.com>
2022-08-10 01:12:20 -0500

    Better TAA clip color

--- shaders/lib/fast_taa.glsl
+++ shaders/lib/fast_taa.glsl
@@ -4,6 +4,21 @@ Temporal antialiasing functions.
 Javier Garduño - GNU Lesser General Public License v3.0
 */
 
+vec3 clip_color(vec3 nmin, vec3 nmax, vec3 past) {
+  vec3 center = (nmin + nmax) * 0.5;
+  float radio = length(nmax - center);
+
+  vec3 color_vector = past - center;
+  float color_dist = length(color_vector);
+
+  float factor = 1.0;
+  if (color_dist > (radio)) {
+    factor =  (radio) / color_dist;
+  }
+
+  return center + (color_vector * factor);
+}
+
 vec3 fast_taa(vec3 current_color, vec2 texcoord_past, vec2 velocity, float pixel_size_x, float pixel_size_y) {
   // Verificamos si proyección queda fuera de la pantalla actual
   if (clamp(texcoord_past, 0.0, 1.0) != texcoord_past) {
@@ -26,7 +41,20 @@ vec3 fast_taa(vec3 current_color, vec2 texcoord_past, vec2 velocity, float pixel
 
     // Muestra del pasado
     vec3 previous = texture2D(colortex3, texcoord_past).rgb;
-    vec3 past_sample = clamp(previous, nmin, nmax);
+    // vec3 past_sample = clamp(previous, nmin, nmax);
+
+    vec3 center = (nmin + nmax) * 0.5;
+    float radio = length(nmax - center);
+
+    vec3 color_vector = previous - center;
+    float color_dist = length(color_vector);
+
+    float factor = 1.0;
+    if (color_dist > radio) {
+      factor = radio / color_dist;
+    }
+
+    vec3 past_sample = center + (color_vector * factor);
 
     // Edge detection
     vec3 edge_color = -neighbourhood[0];
@@ -64,7 +92,20 @@ vec4 fast_taa_depth(vec4 current_color, vec2 texcoord_past, vec2 velocity, float
 
     // Muestra del pasado
     vec4 previous = texture2D(colortex3, texcoord_past);
-    vec4 past_sample = clamp(previous, nmin, nmax);
+    // vec4 past_sample = clamp(previous, nmin, nmax);
+
+    vec3 center = (nmin.rgb + nmax.rgb) * 0.5;
+    float radio = length(nmax.rgb - center);
+
+    vec3 color_vector = previous.rgb - center;
+    float color_dist = length(color_vector);
+
+    float factor = 1.0;
+    if (color_dist > radio) {
+      factor = radio / color_dist;
+    }
+
+    vec4 past_sample = vec4(center + (color_vector * factor), previous.a);
 
     // Edge detection
     vec3 edge_color = -neighbourhood[0].rgb;
