28a3a1ae8ab25a2873b06068e23d5223ea374762
Javier Garduño <javierg.cim@gmail.com>
2023-02-18 19:08:06 -0600

    Avance en Docs

--- shaders/lib/dither.glsl
+++ shaders/lib/dither.glsl
@@ -1,6 +1,31 @@
 /* MakeUp - dither.glsl
 Dither and hash functions
 
+There are a multitude of dithers in MakeUp, with different variants.
+
+There are fixed ones (that do not change over time) as well as those that change
+when temporal sampling is active. Of the latter, there are two versions:
+one that uses dither_shift (Minecraft 1.13+) and another that uses frame_mod
+to rotate the dither values.
+
+There are several variants because each one performs better or worse
+depending on the situation in which it is used.
+
+The philosophy of their use is as follows:
+1) use the fastest one possible that still produces acceptable results.
+2) If multiple effects use a dithering and they are in the same step
+of the Optifine/Iris pipeline, then calculate the dithering only once
+and use it in all the effects that need it to avoid redundant calculations.
+
+The variants that change over time have the prefix "shifted".
+
+The variants with the prefix 'eclectic' are perturbed versions of their simpler counterparts.
+They offer good results because they avoid the appearance of repetitive patterns,
+but they require the calculation of a hash to create this perturbation.
+
+There is a function based on a texture, which assumes a size for the texture of 64x64 pixels,
+but there is no such texture currently.
+
 */
 
 #if MC_VERSION >= 11300
@@ -72,7 +97,7 @@ float dither_grad_noise(vec2 frag) {
   return fract(52.9829189 * fract(dot(vec2(0.06711056, 0.00583715), frag)));
 }
 
-float eclectic_dither(vec2 frag) {
+float eclectic_dither_grad_noise(vec2 frag) {
   vec2 v = 0.0002314814814814815 * frag + vec2(0.25, 0.0);
   float state = fract(dot(v * v, vec2(3571.0)));
   float p4 = fract(state * state * 7142.0) * 0.15;
@@ -160,7 +185,7 @@ float eclectic_makeup_dither(vec2 frag) {
     return fract(dither_shift + (52.9829189 * fract(dot(vec2(0.06711056, 0.00583715), frag))));
   }
 
-  float shifted_eclectic_dither(vec2 frag) {
+  float shifted_eclectic_dither_grad_noise(vec2 frag) {
     vec2 v = 0.0002314814814814815 * frag + vec2(0.25, 0.0);
     float state = fract(dot(v * v, vec2(3571.0)));
     float p4 = fract(state * state * 7142.0) * 0.15;
@@ -240,7 +265,7 @@ float eclectic_makeup_dither(vec2 frag) {
     return fract((frame_mod * 0.4) + (52.9829189 * fract(dot(vec2(0.06711056, 0.00583715), frag))));
   }
 
-  float shifted_eclectic_dither(vec2 frag) {
+  float shifted_eclectic_dither_grad_noise(vec2 frag) {
     vec2 v = 0.0002314814814814815 * frag + vec2(0.25, 0.0);
     float state = fract(dot(v * v, vec2(3571.0)));
     float p4 = fract(state * state * 7142.0) * 0.15;
