7920ba5fbafcac41b472ee11febd882380af4632
Javier Garduño <javierg.cim@gmail.com>
2025-06-28 13:21:47 -0600

    Version 9.1f

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MakeUp - Code
 High performance Minecraft shader (Java).
 
-## Version: 9.1e
+## Version: 9.1f
 
 ### This version works in Optifine and Iris 1.5.1 or higher.
 
--- shaders/src/cloudfinalcolor.glsl
+++ shaders/src/cloudfinalcolor.glsl
@@ -1,6 +1,16 @@
-block_color.rgb =
-    mix(
-        block_color.rgb,
-        texture2D(gaux4, gl_FragCoord.xy * vec2(pixel_size_x, pixel_size_y)).rgb,
-        clamp(pow(gl_FogFragCoord / (far * 1.66), 1.5), 0.0, 1.0)
-    );
+
+#if MC_VERSION < 12106
+    block_color.rgb =
+        mix(
+            block_color.rgb,
+            texture2D(gaux4, gl_FragCoord.xy * vec2(pixel_size_x, pixel_size_y)).rgb,
+            clamp(pow(gl_FogFragCoord / (far * 1.66), 1.5), 0.0, 1.0)
+        );
+#else
+    block_color.rgb =
+        mix(
+            block_color.rgb,
+            texture2D(gaux4, gl_FragCoord.xy * vec2(pixel_size_x, pixel_size_y)).rgb,
+            clamp(pow(gl_FogFragCoord / (2000.0), 1.5), 0.0, 1.0)
+        );
+#endif
